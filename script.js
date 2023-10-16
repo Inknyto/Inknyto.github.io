a9fc22c842f9813452f145d4c03360bc893fb66e
Inknyto <nytosene@gmail.com>
2023-10-16 06:29:39 +0000

    Improved

--- script.js
+++ script.js
@@ -60,9 +60,49 @@ document.addEventListener("DOMContentLoaded", function() {
             newRow.insertCell(3).textContent = durationExtras ? JSON.stringify(durationExtras) : "N/A";
             newRow.insertCell(4).textContent = price ? Math.floor(price / (duration * 30)) : "N/A";
         } else {
-            alert(`Cette durée est impossible: ${duration} mois
-            Choisissez une durée entre 4 et 9 mois
+            alert(`Cette durée est impossible: ${duration} mois. Choisissez une durée entre 4 et 9 mois
             `)
         }
     }
 });
+
+document.getElementById("clear").addEventListener("click", () => {
+
+    const tab = document.querySelector('table');
+    tab.innerHTML = `<tr id="header">
+                        <th>Duration</th>
+                        <th>Mapped Price</th>
+                        <th>Sprints</th>
+                        <th>Extras</th>
+                        <th>Daily</th>
+                    </tr>`
+})
+
+function makeResponsive() {
+    if (window.innerWidth <= 768) {
+      // Your responsive JavaScript code here
+          // Function to display the result in the HTML table
+    function displayResult(duration, price, durationExtras) {
+        const newRow = resultTable.insertRow(-1);
+
+        if (duration >= 4 && duration <= 9) {
+            newRow.insertCell(0).textContent = `Duration : ${duration} months`;
+            newRow.insertCell(1).textContent = price ? Math.floor(price) : "N/A";
+            newRow.insertCell(2).textContent = price ? `${Math.floor(price / (duration * 2))} per sprint for ${duration * 2} sprints` : "N/A";
+        } else {
+            alert(`Cette durée est impossible: ${duration} mois. Choisissez une durée entre 4 et 9 mois
+            `)
+        }
+    }
+      // This code will execute when the screen width is 768 pixels or less
+    } else {
+      // Code to revert changes if needed
+      // This code will execute when the screen width is larger than 768 pixels
+    }
+  }
+  
+  // Add an event listener to check for changes in the window's width
+  window.addEventListener('resize', makeResponsive);
+  
+  // Call the function initially to apply responsive behavior on page load
+  makeResponsive();  
